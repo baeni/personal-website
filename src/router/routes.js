63b1e72cdc0f8a59284939255b9e7e13dcad0e03
baeni <baeni.saa@gmail.com>
2021-09-15 17:53:52 +0200

    Add "terms-of-service", "privacy-policy" & "imprint" paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,10 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
 
+      { path: 'terms-of-service', component: () => import('pages/TermsOfServicePage.vue') },
+      { path: 'privacy-policy', component: () => import('pages/PrivacyPolicyPage.vue') },
+      { path: 'imprint', component: () => import('pages/TermsOfServicePage.vue') },
+
       // catch 404
       { path: ':catchAll(.*)*', component: () => import('pages/Error404Page.vue') }
     ]
