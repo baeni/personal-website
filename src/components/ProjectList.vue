8f3eada77e1f633948bbf203b1b36b5d2c188edb
baeni <baeni.saa@gmail.com>
2021-09-15 17:56:10 +0200

    Redesign

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,17 +1,7 @@
 <template>
-  <q-input class="q-pb-xl" v-model="searchQuery" :label="$t('indexPage.portfolio.formSearch')" bottom-slots dark dense standout clearable clear-icon="close">
-    <template v-slot:prepend>
-      <q-icon name="search" />
-    </template>
-
-    <template v-slot:hint>
-      {{ $t('indexPage.portfolio.formSearchHint') }}
-    </template>
-  </q-input>
-
   <div class="row q-col-gutter-md">
     <project-list-item
-      v-for="(project, i) in queryProjects"
+      v-for="(project, i) in projects"
       :key="i"
       :project="project"
     />
@@ -30,30 +20,6 @@ export default {
     projects: {
       type: Array,
       required: true
-    }
-  },
-  data() {
-    return {
-      searchQuery: ''
-    }
-  },
-  computed: {
-    queryProjects() {
-      if (this.searchQuery) {
-        const words = this.searchQuery.toLowerCase().split(', ').join(',').split(",").filter(word => {
-          if (word) return word;
-        });
-
-        return this.projects.filter(project => {
-          return project.tags.some(tag => {
-            return words.some(word => {
-              return tag.includes(word);
-            })
-          });
-        });
-      } else {
-        return this.projects;
-      }
     }
   }
 }
