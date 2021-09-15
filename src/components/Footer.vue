9fecba96facf1a3bd82ce50f41697ad40d54dfa9
baeni <baeni.saa@gmail.com>
2021-09-15 17:53:12 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-footer class="flex">
+    <q-toolbar class="container">
+      <q-space />
+
+      <router-link
+        class="text-bold text-lightest"
+        style="text-decoration: none;"
+        to="terms-of-service"
+      >
+        Terms of service
+      </router-link>
+
+      <q-space />
+
+      <router-link
+        class="text-bold text-lightest"
+        style="text-decoration: none;"
+        to="privacy-policy"
+      >
+        Privacy policy
+      </router-link>
+
+      <q-space />
+
+      <router-link
+        class="text-bold text-lightest"
+        style="text-decoration: none;"
+        to="imprint"
+      >
+        Imprint
+      </router-link>
+
+      <q-space />
+
+      <div class="text-bold text-light">&copy; 2021 baeni</div>
+
+      <q-space />
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  min-height: 75px;
+  background: transparentize($light, .9);
+  backdrop-filter: blur(25px);
+}
+</style>
