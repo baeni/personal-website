28525a6603b8f2284cf85456c5252ce0b26f0474
baeni <baeni.saa@gmail.com>
2021-09-15 17:54:15 +0200

    Add `Footer`-component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,16 +5,20 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <footr />
   </q-layout>
 </template>
 
 <script>
 import Navbar from 'components/Navbar';
+import Footr from 'components/Footer';
 
 export default {
   name: 'MainLayout',
   components: {
-    Navbar
+    Navbar,
+    Footr
   }
 }
 </script>
