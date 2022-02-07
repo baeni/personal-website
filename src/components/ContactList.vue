4e087f9f6d3a65b5d43ec5325f5b381a87ec7199
Benjamin Saalfeld <baeni.saa@gmail.com>
2022-02-08 00:12:01 +0100

    Add link to mail

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="row q-col-gutter-lg">
+  <div class="row q-col-gutter-lg items-center">
     <contact-list-item
       v-for="(contact, i) in contacts"
       :key="i"
       :contact="contact"
     />
+    <a class="text-light text-h4" style="text-decoration: none;" href="mailto:hello@baeni.de?subject=Hi baeni!">{{ $t('indexPage.landing.mailMe') }}</a>
   </div>
 </template>
 
