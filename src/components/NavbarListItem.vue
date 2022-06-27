dbeabf744cf828297133064071403e1e7bb11cf9
Benjamin Saalfeld <baeni.saa@gmail.com>
2022-06-28 00:42:42 +0200

    Create NavbarListItem.vue

---
+++ src/components/NavbarListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-btn
+    class="text-bold"
+    :label="label"
+    no-caps
+    flat
+    @click="toId(targetPath != undefined ? targetPath : '/', targetHash)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'NavbarListItem',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    targetPath: {
+      type: String
+    },
+    targetHash: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    async toId(path, hash) {
+      await this.$router.push(path);
+      await document.getElementById(hash).scrollIntoView();
+    }
+  }
+}
+</script>
