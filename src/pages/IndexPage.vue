ebc7417898e211554d85e4201715b32eb5d0e7ca
Benjamin Saalfeld <baeni.saa@gmail.com>
2022-06-28 00:41:56 +0200

    Minor changes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page>
     <!-- landing -->
-    <section class="flex window-height">
+    <section id="landing" class="flex window-height">
       <q-img class="absolute-top full-width window-height" style="z-index: -99; opacity: .075;" src="~assets/background.jpg" />
       <div class="container row q-col-gutter-x-xl">
         <q-img v-if="$q.screen.gt.xs" class="col-2" fit="contain" src="https://brand.baeni.de/logo-blue@256.png" />
         <div class="col-10 q-my-auto">
+          <div class="text-h1 text-white text-hammersmith-one">{{ $t('indexPage.landing.heading') }}</div>
           <div class="text-subtitle1 text-lightest">
-            <span class="text-h1 text-white text-hammersmith-one">{{ $t('indexPage.landing.heading') }}</span>
             {{ $t('indexPage.landing.subtitle') }}
           </div>
           <contact-list class="q-pt-lg" :contacts="contacts" />
@@ -60,8 +60,8 @@ const contacts = [
 export default {
   name: 'IndexPage',
   components: {
-    ProjectList,
-    ContactList
+    ContactList,
+    ProjectList
   },
   data() {
     return {
