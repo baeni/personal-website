93ac8cd2519171ea8561c224efdcc8ec14671ac0
Benjamin Saalfeld <baeni.saa@gmail.com>
2022-06-28 00:40:13 +0200

    Adjustments related to i18n

--- src/pages/Error404Page.vue
+++ src/pages/Error404Page.vue
@@ -4,8 +4,8 @@
     <section class="flex flex-center window-height">
       <q-img class="absolute-top full-width window-height" style="z-index: -99; opacity: .65;" src="~assets/kermit.gif" autoplay loop />
       <div class="container text-center">
-        <div class="text-h1 text-uppercase text-lightest">{{ $t('error404Page.heading') }}</div>
-        <div class="text-subtitle1 text-lightest">{{ $t('error404Page.description') }}</div>
+        <div class="text-h1 text-uppercase text-lightest">{{ $t('error404Page.landing.heading') }}</div>
+        <div class="text-subtitle1 text-lightest">{{ $t('error404Page.landing.subtitle') }}</div>
 
         <q-btn class="text-bold bg-lightest q-mt-xl" :label="$t('indexPage.name')" flat @click="$router.push('/')" />
       </div>
