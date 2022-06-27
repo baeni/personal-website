cac0d2c518268838e54d839489fd85ec827d8502
Benjamin Saalfeld <baeni.saa@gmail.com>
2022-06-28 00:42:48 +0200

    Create NavbarList.vue

---
+++ src/components/NavbarList.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-toolbar class="text-white">
+    <navbar-list-item :label="$t('indexPage.landing.heading')" targetHash="landing" />
+    <navbar-list-item :label="$t('indexPage.projects.heading')" targetHash="projects" />
+  </q-toolbar>
+</template>
+
+<script>
+import NavbarListItem from './NavbarListItem'
+
+export default {
+  name: 'NavbarList',
+  components: {
+    NavbarListItem
+  }
+}
+</script>
